039382b14ac454a3d7fc96678f197324283ad818
Mayank Mishra <mishramayank101005@gmail.com>
2026-03-02 22:26:03 +0530

    EDA and problem statement

--- src/exception.py
+++ src/exception.py
@@ -19,10 +19,4 @@ class CustomException(Exception):
     
 
 
-if __name__=="__main__":
 
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divide by zero",exc_info=True)
-        raise CustomException(e,sys)
--- src/logger.py
+++ src/logger.py
@@ -1,31 +1,14 @@
-# import logging
-# import os
-# from datetime import datetime
-
-# LOG_FILE=f"{datetime.now().strftime('%m_%D_%Y_%H_%M_%S')}.log"
-# logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-# os.makedirs(logs_path,exist_ok=True)
-
-# LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
-
-# import logging
-
-# logging.basicConfig(
-#     filename=LOG_FILE_PATH,
-#     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-#     level=logging.INFO,
-# )
-
 import logging
 import os
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE=f"{datetime.now().strftime('%m_%D_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
 
-logs_folder = os.path.join(os.getcwd(), "logs")
-os.makedirs(logs_folder, exist_ok=True)
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
-LOG_FILE_PATH = os.path.join(logs_folder, LOG_FILE)
+import logging
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
@@ -33,4 +16,4 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
-print("Logger configured. File:", LOG_FILE_PATH)
+
